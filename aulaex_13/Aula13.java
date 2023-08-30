097e8a5debf0754f700de63c5078f6a4690c37ba
Ana Barbosa <141193365+barbosx7@users.noreply.github.com>
2023-08-29 21:50:29 -0300

    Add files via upload

---
+++ aulaex_13/Aula13.java
@@ -0,0 +1,25 @@
+
+package aulaex_13;
+
+import java.util.Scanner;
+
+/**
+ * Exercício 13
+ * Aula: 22/08/2023
+ * Disciplina: Linguagem de Programação
+ * @author Ana Luísa Chagas Barbosa
+ */
+public class Aula13 {
+    public static void main (String[] args) {
+        Scanner ler = new Scanner (System.in);
+        int numero, resto;
+        
+        
+        System.out.println("Digite um numero inteiro");
+        numero = ler.nextInt();
+        
+        resto = numero % 2;
+        
+        if (resto != 0)
+    }
+}
---
+++ aulaex_16/Usuário.java
@@ -0,0 +1,33 @@
+
+package aulaex_16;
+
+/**
+ * Exercício 16
+ * Aula: 29/08/2023
+ * Disciplina: Linguagem de Programação
+ * Materia: Tela de Login
+ * @author Ana Luísa Chagas Barbosa
+ */
+public class Usuário 
+{
+    private String nome = "Adm";
+    private String senha = "A777";
+            
+      public String getNome ()  {
+          return nome;
+      }
+      
+      public void setNome(String nome) {
+          this.nome = nome;
+      }
+      
+      public String getSenha ()  {
+          return senha;
+      }
+      
+      public void setSenha(String senha) {
+          this.senha = senha;
+      }
+            
+    
+}
